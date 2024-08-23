0c15459e279d70d0e066837444dff0654f49c99b
Dhasarathan <dhasarathandharmaraj@gmail.com>
2024-08-23 17:57:50 +0530

    navigate cars page

--- src/components/UI/FindCarForm.jsx
+++ src/components/UI/FindCarForm.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import "../../styles/find-car-form.css";
 import "../../styles/find-car-form.css";
 import { Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 // import { Form,} from "reactstrap";
 const FindCarForm = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/cars");
+  };
   return (
     <Form className="form">
       <div className=" d-flex align-items-center justify-content-between flex-wrap">
@@ -30,11 +37,14 @@ const FindCarForm = () => {
             required
           />
         </FormGroup>
-     
+
         <FormGroup className="form__group ">
-        <label htmlFor="journeyDate" className="form__label text-white">
-        J          </label>
-          <button className="btn find__car-btn  ">Find Now</button>
+          <label htmlFor="journeyDate" className="form__label text-white">
+            J{" "}
+          </label>
+          <button className="btn find__car-btn" onClick={handleClick}>
+            Find Now
+          </button>
         </FormGroup>
       </div>
     </Form>
