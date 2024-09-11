ad11f46014990011d671353ee59127b4c0d64ee6
Dhasarathan <dhasarathandharmaraj@gmail.com>
2024-09-11 12:31:16 +0530

    update

--- src/components/UI/Fulfillment.jsx
+++ src/components/UI/Fulfillment.jsx
@@ -100,89 +100,87 @@ const Fulfillment = () => {
 
   return (
     <>
-      <div className="pb-5 pt-5">
-        <div className="d-flex date_container flex-row">
-          <div className="form-groups me-3">
-            <label htmlFor="startDate">Start Date & Time</label>
+      <div className="d-flex date_container flex-row">
+        <div className="form-groups me-3">
+          <label htmlFor="startDate">Start Date & Time</label>
+          <input
+            type="datetime-local"
+            id="startDate"
+            className="form-control"
+            value={startDate}
+            min={today} // Prevent past dates
+            onChange={handleStartDateChange}
+          />
+        </div>
+
+        <div className="form-groups">
+          <label htmlFor="endDate">End Date & Time</label>
+          <input
+            type="datetime-local"
+            id="endDate"
+            className="form-control"
+            value={endDate}
+            min={startDate} // Prevent reverse dates
+            onChange={handleEndDateChange}
+          />
+        </div>
+      </div>
+      <div className="fulfillment-container">
+        <div className="radio-buttons">
+          <label>
             <input
-              type="datetime-local"
-              id="startDate"
-              className="form-control"
-              value={startDate}
-              min={today} // Prevent past dates
-              onChange={handleStartDateChange}
+              type="checkbox"
+              name="selfPickup"
+              checked={selectedOption === "selfPickup"}
+              onChange={handleCheckboxChange}
+            />
+            Self-Pickup
+          </label>
+          <label>
+            <input
+              type="checkbox"
+              name="delivery"
+              checked={selectedOption === "delivery"}
+              onChange={handleCheckboxChange}
+            />
+            Delivery
+          </label>
+        </div>
+
+        {selectedOption === "selfPickup" && (
+          <div className="select-input-container">
+            <select className="select-input">
+              <option value="">Select an option</option>
+              <option value="option1">Kilampakkam</option>
+              <option value="option2">Koyambedu</option>
+              <option value="option3">Tambaram</option>
+            </select>
+          </div>
+        )}
+
+        {selectedOption === "delivery" && (
+          <div className="delivery-info-container">
+            <input
+              type="text"
+              className="input-fieldss"
+              placeholder="Enter delivery address"
+              value={deliveryInfo}
+              onChange={handleDeliveryInfoChange}
+            />
+            <textarea
+              className="text-areas"
+              placeholder="Enter delivery instructions or additional details"
+              value={extraInfo}
+              onChange={handleExtraInfoChange}
             />
           </div>
+        )}
 
-          <div className="form-groups">
-            <label htmlFor="endDate">End Date & Time</label>
-            <input
-              type="datetime-local"
-              id="endDate"
-              className="form-control"
-              value={endDate}
-              min={startDate} // Prevent reverse dates
-              onChange={handleEndDateChange}
-            />
-          </div>
-        </div>
-        <div className="fulfillment-container">
-          <div className="radio-buttons">
-            <label>
-              <input
-                type="checkbox"
-                name="selfPickup"
-                checked={selectedOption === "selfPickup"}
-                onChange={handleCheckboxChange}
-              />
-              Self-Pickup
-            </label>
-            <label>
-              <input
-                type="checkbox"
-                name="delivery"
-                checked={selectedOption === "delivery"}
-                onChange={handleCheckboxChange}
-              />
-              Delivery
-            </label>
-          </div>
-
-          {selectedOption === "selfPickup" && (
-            <div className="select-input-container">
-              <select className="select-input">
-                <option value="">Select an option</option>
-                <option value="option1">Kilampakkam</option>
-                <option value="option2">Koyambedu</option>
-                <option value="option3">Tambaram</option>
-              </select>
-            </div>
-          )}
-
-          {selectedOption === "delivery" && (
-            <div className="delivery-info-container">
-              <input
-                type="text"
-                className="input-fieldss"
-                placeholder="Enter delivery address"
-                value={deliveryInfo}
-                onChange={handleDeliveryInfoChange}
-              />
-              <textarea
-                className="text-areas"
-                placeholder="Enter delivery instructions or additional details"
-                value={extraInfo}
-                onChange={handleExtraInfoChange}
-              />
-            </div>
-          )}
-
-          <p className="disclaimer">
-            <strong>Disclaimer :</strong> Delivery Charges may vary for outside
-            city limits locations including Airport pickup/drop. The same will
-            be confirmed upon KYC verification.
-          </p>
-        </div>
+        <p className="disclaimer">
+          <strong>Disclaimer :</strong> Delivery Charges may vary for outside
+          city limits locations including Airport pickup/drop. The same will be
+          confirmed upon KYC verification.
+        </p>
       </div>
     </>
   );
