60a4bce2fd5f8083c26d5f70b8f461b402b8482f
Dhasarathan <dhasarathandharmaraj@gmail.com>
2024-09-14 18:12:56 +0530

    update

--- src/pages/UploadCheckButton.jsx
+++ src/pages/UploadCheckButton.jsx
@@ -1,27 +1,38 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import UploadConfirm from "./UploadConfirm"; // Adjust the import path as necessary
 import FileUpload from "./FileUpload"; // Adjust the import path as necessary
 
 const UploadCheckButton = () => {
   const [hasUploadedData, setHasUploadedData] = useState(false); // State to determine if data is uploaded
   const BASE_URL = process.env.REACT_APP_BACKEND_URL;
-  const id = localStorage.getItem("id");
+  const customerId = localStorage.getItem("id");
 
   useEffect(() => {
     const checkUploadStatus = async () => {
       try {
-        const response = await axios.get(`${BASE_URL}/getCustomerRentCarBookedDocumentVerificationCheckStatus`, {
-          params: { id }, // Sending userid as a query parameter
-        });
-        setHasUploadedData(response.data.hasUploadedData); // Adjust based on your API response
+        var jsonObj = JSON.parse("{}");
+        jsonObj["id"] = customerId;
+        const response = await fetch(
+          `${BASE_URL}/getCustomerRentCarBookedDocumentVerificationCheckStatus`,
+          {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(jsonObj),
+          }
+        );
+
+        // Convert response to JSON
+        const data = await response.json();
+
+        // Assuming the response JSON structure is { hasUploadedData: boolean }
+        setHasUploadedData(data.hasUploadedData);
       } catch (error) {
         console.error("Error checking upload status:", error);
       }
     };
 
     checkUploadStatus();
-  }, [BASE_URL, id]); // Add userid as a dependency
+  }, [BASE_URL, customerId]); // Add customerId as a dependency
 
   const handleUploadComplete = (success) => {
     // Handle upload completion (e.g., refresh the status or show a notification)
